a7634c9900d76c0272ded88129e2095298a7f2b8
abdullah-al-noman <nomansdropbox@gmail.com>
2020-09-09 16:53:37 +0600

    initial commit: template edited in grails env

---
+++ grails-app/controllers/studentmanagement/HomeController.groovy
@@ -0,0 +1,14 @@
+package studentmanagement
+
+class HomeController {
+
+    def index() { }
+    def menu(){}
+    def about(){}
+    def blog(){}
+    def blogdetails(){}
+    def contact(){}
+    def gallery(){}
+    def reservation(){}
+    def stuff(){}
+}
---
+++ grails-app/init/restaurent/BootStrap.groovy
@@ -0,0 +1,9 @@
+package restaurent
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
